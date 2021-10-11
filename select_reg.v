396b2a05ff00b04494bcb40b9a524c1fa950cfab
cebarobot <ceba_robot@outlook.com>
2021-10-11 10:07:24 +0800

    feat: add select & start registers

---
+++ select_reg.v
@@ -0,0 +1,39 @@
+module select_reg (
+    input  clk,
+    input  resetn,
+
+    input  do_arr_reg_select,
+    input  [11:0] arr_reg_select_data,
+
+    input  start_to_select_enable,
+    input  [11:0] reg_start_value,
+
+    input  addr1_to_select_enable,
+    input  [11:0] addr1_value,
+
+    input  addr2_to_select_enable,
+    input  [11:0] addr2_value,
+
+    output [11:0] reg_select_value
+);
+
+reg [11:0] reg_select;
+
+always @(posedge clk) begin
+    if (~resetn) begin
+        reg_select <= 0;
+    end else if (do_arr_reg_select) begin
+        reg_select <= arr_reg_select_data;
+    end else if (start_to_select_enable) begin
+        reg_select <= reg_start_value;
+    end else if (addr1_to_select_enable) begin
+        reg_select <= addr1_value;
+    end else if (addr2_to_select_enable) begin
+        reg_select <= addr2_value;
+    end
+end
+
+assign reg_select_value = reg_select;
+
+
+endmodule
---
+++ start_reg.v
@@ -0,0 +1,33 @@
+module start_reg (
+    input  clk,
+    input  resetn,
+
+    input  do_arr_reg_start,
+    input  [11:0] arr_reg_start_data,
+
+    input  do_reg_start_inc,
+
+    input  do_mod_reg_start,
+    input  [11:0] mod_reg_start_data,
+
+    output [11:0] reg_start_value
+);
+
+reg [11:0] reg_start;
+
+always @(posedge clk) begin
+    if (~resetn) begin
+        reg_start <= 0;
+    end else if (do_arr_reg_start) begin
+        reg_start <= arr_reg_start_data;
+    end else if (do_reg_start_inc) begin
+        reg_start <= reg_start + 12'o1;
+    end else if (do_mod_reg_start) begin
+        reg_start <= mod_reg_start_data;
+    end
+end
+
+assign reg_start_value = reg_start;
+
+
+endmodule
