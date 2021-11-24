d810583daccd47f633f86ba27bee0a63d7f553cb
cebarobot <ceba_robot@outlook.com>
2021-11-25 07:19:08 +0800

    feat: pulse unit support change signal according to operator, without test

--- operator.v
+++ operator.v
@@ -2,28 +2,64 @@ module operator (
     input  clk,
     input  resetn,
 
-    output read_addr2,
-    output write_addr2,
-    output arth_and,
+    input  reg_a_sign,
+    input  reg_b_sign,
 
-    input  op_code_valid,
-    input  [5:0] op_code_data
+    output [ 5:0] pulse_unit_ctrl,
+
+    input  do_c_to_operator,
+    input  [ 5:0] c_to_operator_value,
+
+    output [ 5:0] op_code_value
 );
 
-reg [5:0] op_code;
+reg  [ 5:0] op_code;
+
+wire [ 2:0] op_code_p1;
+wire [ 2:0] op_code_p2;
+
+wire ctrl_abs;
+
+wire ctrl_select_to_start_at_4;
+wire ctrl_select_to_start_at_7;
+wire ctrl_move_b_to_c_at_7;         // opp: ctrl_move_c_to_b_at_7
+wire ctrl_mem_read_at_3;
+wire ctrl_mem_rw_at_5;
+wire ctrl_mem_write_at_5;
 
 always @ (posedge clk) begin
     if (~resetn) begin
         op_code <= 6'h0;
-    end else if (op_code_valid) begin
-        op_code <= op_code_data;
+    end else if (do_c_to_operator) begin
+        op_code <= c_to_operator_value;
     end
 end
 
-assign read_addr2   = op_code[4] == 1'b0;
-assign write_addr2  = op_code[3] == 1'b0;
-assign arth_and     = op_code[2:0] == 3'o6;
+assign op_code_value = op_code;
+assign op_code_p1 = op_code[5:3];
+assign op_code_p2 = op_code[2:0];
 
-// TODO:
+// control line for operator & arith
+assign ctrl_abs         = op_code_p1[2] && op_code_p1[0];
+assign ctrl_write_res   = ~op_code_p1[0];
+assign ctrl_print_out   = ~op_code_p1[0] && op_code_p1[2];
+assign ctrl_stop        = 1'b0;
+
+// control line for pulse_unit unit
+assign ctrl_select_to_start_at_4    = op_code_p2 == 3'o4 && op_code_p1[1];
+assign ctrl_select_to_start_at_7    = op_code_p2 == 3'o4 && op_code_p1[1] && op_code_p1[0] && ~reg_b_sign;
+assign ctrl_move_b_to_c_at_7        = op_code_p1[1];
+assign ctrl_mem_read_at_3           = op_code_p2 == 3'o4;
+assign ctrl_mem_rw_at_5             = ~op_code_p1[1];
+assign ctrl_mem_write_at_5          = op_code_p2 == 3'o5;
+
+assign pulse_unit_ctrl = {
+    ctrl_select_to_start_at_4,
+    ctrl_select_to_start_at_7,
+    ctrl_move_b_to_c_at_7,
+    ctrl_mem_read_at_3,
+    ctrl_mem_rw_at_5,
+    ctrl_mem_write_at_5
+};
 
 endmodule
--- pulse_unit.v
+++ pulse_unit.v
@@ -3,13 +3,15 @@ module pulse_unit (
     input resetn,
     
     output do_c_to_operator,
+    output do_start_inc,
     output do_addr1_to_select,
     output do_addr2_to_select,
     output do_start_to_select,
+    output do_select_to_start,
     output do_mem_to_c,
-    output do_start_inc,
     output do_move_c_to_a,
     output do_move_c_to_b,
+    output do_move_b_to_c,
 
     output mem_read_pulse,
     output mem_write_pulse,
@@ -18,18 +20,44 @@ module pulse_unit (
     output operate_pulse,
     input  operate_reply,
 
-    input  start_pulse
+    input  start_pulse,
+
+    input  [ 5:0] pulse_unit_ctrl
 );
 
 reg [ 2:0] cur_pulse;
 reg [ 2:0] next_pulse;
 
 wire [7:0] do_pulse;
+// wire do_pulse;
 wire [7:0] at_pulse;
 wire [7:0] entering_pulse;
 
 wire outer_do_pulse;
 reg  outer_do_pulse_delay;
+
+wire ctrl_select_to_start_at_4;
+wire ctrl_select_to_start_at_7;
+wire ctrl_move_b_to_c_at_7;         // opp: ctrl_move_c_to_b_at_7
+wire ctrl_move_c_to_b_at_7;         // new
+wire ctrl_mem_read_at_3;
+wire ctrl_mem_rw_at_5;
+wire ctrl_mem_write_at_5;           // opp: ctrl_mem_read_at_5
+wire ctrl_mem_read_at_5;            // new
+
+// pulse ctrl
+
+assign {
+    ctrl_select_to_start_at_4,
+    ctrl_select_to_start_at_7,
+    ctrl_move_b_to_c_at_7,
+    ctrl_mem_read_at_3,
+    ctrl_mem_rw_at_5,
+    ctrl_mem_write_at_5
+} = pulse_unit_ctrl;
+
+assign ctrl_move_c_to_b_at_7 = !ctrl_move_b_to_c_at_7;
+assign ctrl_mem_read_at_5    = !ctrl_mem_read_at_5;
 
 // state machine of pulse
 
@@ -58,6 +86,15 @@ assign at_pulse[5] = cur_pulse == 3'o5;
 assign at_pulse[6] = cur_pulse == 3'o6;
 assign at_pulse[7] = cur_pulse == 3'o7;
 
+// assign entering_pulse[1] = at_pulse[0] && do_pulse;
+// assign entering_pulse[2] = at_pulse[1] && do_pulse;
+// assign entering_pulse[3] = at_pulse[2] && do_pulse;
+// assign entering_pulse[4] = at_pulse[3] && do_pulse;
+// assign entering_pulse[5] = at_pulse[4] && do_pulse;
+// assign entering_pulse[6] = at_pulse[5] && do_pulse;
+// assign entering_pulse[7] = at_pulse[6] && do_pulse;
+// assign entering_pulse[0] = at_pulse[7] && do_pulse;
+
 assign entering_pulse[1] = do_pulse[0];
 assign entering_pulse[2] = do_pulse[1];
 assign entering_pulse[3] = do_pulse[2];
@@ -67,13 +104,15 @@ assign entering_pulse[6] = do_pulse[5];
 assign entering_pulse[7] = do_pulse[6];
 assign entering_pulse[0] = do_pulse[7];
 
+// assign do_pulse = cur_pulse[0] ? 1'b1 : outer_do_pulse_delay;
+
 assign do_pulse[0] = at_pulse[0] && outer_do_pulse_delay;
 assign do_pulse[1] = at_pulse[1];
 assign do_pulse[2] = at_pulse[2] && outer_do_pulse_delay;
 assign do_pulse[3] = at_pulse[3];
-assign do_pulse[4] = at_pulse[4] && outer_do_pulse_delay;
+assign do_pulse[4] = at_pulse[4] && (outer_do_pulse_delay || !ctrl_mem_read_at_3);
 assign do_pulse[5] = at_pulse[5];
-assign do_pulse[6] = at_pulse[6] && outer_do_pulse_delay;
+assign do_pulse[6] = at_pulse[6] && (outer_do_pulse_delay || !ctrl_mem_rw_at_5);
 assign do_pulse[7] = at_pulse[7];
 
 // outer pulse delay
@@ -91,22 +130,31 @@ end
 // output control signals 
 // TODO: need to change output according to input from operator (instruction decoder)
 
+assign do_c_to_operator     = entering_pulse[3];
 assign do_start_inc         = entering_pulse[3];
 assign do_addr1_to_select   = entering_pulse[3];
-assign do_addr2_to_select   = at_pulse[4] && mem_reply;
+assign do_addr2_to_select   = 
+    (at_pulse[4] && mem_reply && ctrl_mem_read_at_3) ||
+    (entering_pulse[4]        && !ctrl_mem_read_at_3);
 assign do_start_to_select   = entering_pulse[1];
+assign do_select_to_start   = 
+    (entering_pulse[4] && ctrl_select_to_start_at_4) ||
+    (entering_pulse[7] && ctrl_select_to_start_at_7);
 assign do_move_c_to_a       = entering_pulse[5];
-assign do_move_c_to_b       = entering_pulse[7];
+assign do_move_c_to_b       = 
+    (entering_pulse[7] && ctrl_move_c_to_b_at_7);
+assign do_move_b_to_c       = 
+    (entering_pulse[7] && ctrl_move_b_to_c_at_7);
 assign do_mem_to_c          = 
     (at_pulse[2] && mem_reply) || 
-    (at_pulse[4] && mem_reply) || 
-    (at_pulse[6] && mem_reply);
-assign do_c_to_operator     = entering_pulse[3];
+    (at_pulse[4] && mem_reply && ctrl_mem_read_at_3) || 
+    (at_pulse[6] && mem_reply && ctrl_mem_rw_at_5 && ctrl_mem_read_at_5);
 assign mem_read_pulse       = 
-    at_pulse[1] ||
-    at_pulse[3] || 
-    at_pulse[5];
-assign mem_write_pulse      = 1'b0;
+    (at_pulse[1]) ||
+    (at_pulse[3] && ctrl_mem_read_at_3) || 
+    (at_pulse[5] && ctrl_mem_rw_at_5 && ctrl_mem_read_at_5);
+assign mem_write_pulse      = 
+    (at_pulse[5] && ctrl_mem_rw_at_5 && ctrl_mem_write_at_5);
 assign operate_pulse        = at_pulse[7];
 
 endmodule
